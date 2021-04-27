2bf5c384a317a03dd643c3b55f7c40cb4300e052
Jaffer Ali <jafferali150@gmail.com>
2021-04-27 17:39:57 +0530

    v1 books api

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ app.use(express.json())
 
 const books = [
     {title: 'Java Programming', id: 1},
-    {title: 'C# Programming', id: 2}
+    {title: 'C# Programming', id: 2},
+    {title: 'Node JS Programming', id: 3},
 ]
 
 app.get('/', (req, res) => {
     res.send('Books API : REST API with Node JS')
+})
+
+app.get('/api/books', (req, res) => {
+    res.send(books)
 })
 
 app.listen(3030)
